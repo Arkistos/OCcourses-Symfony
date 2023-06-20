a96c5fc41d40134d8bc1a5fe53d6d94443c2b742
Arkistos <pierre.laco@hotmail.fr>
2023-06-20 09:06:45 +0200

    init

---
+++ src/Entity/Orders.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Entity;
+
+use App\Entity\Trait\CreatedAtTrait;
+use App\Repository\OrdersRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: OrdersRepository::class)]
+class Orders
+{
+
+    use CreatedAtTrait;
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 20, unique:true)]
+    private ?string $reference = null;
+
+
+    #[ORM\ManyToOne(inversedBy: 'orders')]
+    private ?Coupons $coupons = null;
+
+    #[ORM\ManyToOne(inversedBy: 'orders')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Users $users = null;
+
+    #[ORM\OneToMany(mappedBy: 'orders', targetEntity: OrdersDetails::class)]
+    private Collection $products;
+
+    public function __construct()
+    {
+        $this->products = new ArrayCollection();
+        $this->created_at = new \DateTimeImmutable();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getReference(): ?string
+    {
+        return $this->reference;
+    }
+
+    public function setReference(string $reference): static
+    {
+        $this->reference = $reference;
+
+        return $this;
+    }
+
+
+    public function getCoupons(): ?Coupons
+    {
+        return $this->coupons;
+    }
+
+    public function setCoupons(?Coupons $coupons): static
+    {
+        $this->coupons = $coupons;
+
+        return $this;
+    }
+
+    public function getUsers(): ?Users
+    {
+        return $this->users;
+    }
+
+    public function setUsers(?Users $users): static
+    {
+        $this->users = $users;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, OrdersDetails>
+     */
+    public function getProducts(): Collection
+    {
+        return $this->products;
+    }
+
+    public function addProduct(OrdersDetails $product): static
+    {
+        if (!$this->products->contains($product)) {
+            $this->products->add($product);
+            $product->setOrders($this);
+        }
+
+        return $this;
+    }
+
+    public function removeProduct(OrdersDetails $product): static
+    {
+        if ($this->products->removeElement($product)) {
+            // set the owning side to null (unless already changed)
+            if ($product->getOrders() === $this) {
+                $product->setOrders(null);
+            }
+        }
+
+        return $this;
+    }
+}
